9ffc310e2dbe5ae2e24af56a3956dd05565613e1
daychongyang <daychongyang@gmail.com>
2021-03-02 18:46:50 +0800

    fix: tests

--- algorithms/typescript/two-sum/two-sum.test.ts
+++ algorithms/typescript/two-sum/two-sum.test.ts
@@ -5,19 +5,19 @@ Deno.test("Two Sum #1", () => {
   const nums = [2, 7, 11, 15];
   const target = 9;
 
-  assertEquals(twoSum(nums, target).toString(), "0,1");
+  assertEquals(twoSum(nums, target).toString(), [0, 1].toString());
 });
 
 Deno.test("Two Sum #2", () => {
   const nums = [3, 2, 4];
   const target = 6;
 
-  assertEquals(twoSum(nums, target).toString(), "1,2");
+  assertEquals(twoSum(nums, target).toString(), [1, 2].toString());
 });
 
 Deno.test("Two Sum #3", () => {
   const nums = [3, 3];
   const target = 6;
 
-  assertEquals(twoSum(nums, target).toString(), "0,1");
+  assertEquals(twoSum(nums, target).toString(), [0, 1].toString());
 });
