1df174f14c7931392066a62b9c835a89a7405f35
daychongyang <daychongyang@gmail.com>
2021-03-04 18:02:03 +0800

    滑动窗口

--- algorithms/longest-substring-without-repeating-characters/main.ts
+++ algorithms/longest-substring-without-repeating-characters/main.ts
@@ -2,20 +2,17 @@
  * @see {@link https://leetcode.com/problems/longest-substring-without-repeating-characters/solution/}
  */
 
-function lengthOfLongestSubstring(s: string): number {
+export function lengthOfLongestSubstring(s: string): number {
   const len = s.length;
-  const set = new Set<string>([]);
+  const map: Record<string, number> = {};
   let longestSubstringLength = 0;
-  let i = 0;
-  let j = 0;
 
-  while (i < len && j < len) {
-    if (!set.has(s.charAt(j))) {
-      set.add(s.charAt(j++));
-      longestSubstringLength = Math.max(longestSubstringLength, j - i);
-    } else {
-      set.delete(s.charAt(i++)); // 字符重复
+  for (let i = 0, j = 0; j < len; j++) {
+    if (map[s.charAt(j)] !== undefined) {
+      longestSubstringLength = Math.max(longestSubstringLength, j - i + 1);
     }
+
+    map[s.charAt(j)] = j + 1;
   }
 
   return longestSubstringLength;
---
+++ algorithms/longest-substring-without-repeating-characters/slide-window.ts
@@ -0,0 +1,22 @@
+/**
+ * @see {@link https://leetcode.com/problems/longest-substring-without-repeating-characters/solution/}
+ */
+
+export function lengthOfLongestSubstring(s: string): number {
+  const len = s.length;
+  const set = new Set<string>([]);
+  let longestSubstringLength = 0;
+  let i = 0;
+  let j = 0;
+
+  while (i < len && j < len) {
+    if (!set.has(s.charAt(j))) {
+      set.add(s.charAt(j++));
+      longestSubstringLength = Math.max(longestSubstringLength, j - i);
+    } else {
+      set.delete(s.charAt(i++)); // 字符重复
+    }
+  }
+
+  return longestSubstringLength;
+}
