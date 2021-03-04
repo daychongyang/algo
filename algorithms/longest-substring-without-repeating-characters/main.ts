231e0f536155670731119de29f646a25141443ed
daychongyang <daychongyang@gmail.com>
2021-03-04 17:19:22 +0800

    最长子串

--- algorithms/longest-substring-without-repeating-characters/main.ts
+++ algorithms/longest-substring-without-repeating-characters/main.ts
@@ -1,27 +1,20 @@
 /**
  * @see {@link https://leetcode.com/problems/longest-substring-without-repeating-characters/solution/}
  */
+
 function lengthOfLongestSubstring(s: string): number {
   const len = s.length;
+  const set = new Set<string>([]);
   let longestSubstringLength = 0;
+  let i = 0;
+  let j = 0;
 
-  for (let i = 0; i < len; i++) {
-    let e = i;
-    while (e < len) {
-      const substring = s.substring(i, e + 1);
-      if (
-        Array.from(substring).some(
-          (char) => substring.lastIndexOf(char) !== substring.indexOf(char)
-        )
-      ) {
-        break;
-      } else {
-        longestSubstringLength = Math.max(
-          longestSubstringLength,
-          substring.length
-        );
-        e += 1;
-      }
+  while (i < len && j < len) {
+    if (!set.has(s.charAt(j))) {
+      set.add(s.charAt(j++));
+      longestSubstringLength = Math.max(longestSubstringLength, j - i);
+    } else {
+      set.delete(s.charAt(i++)); // 字符重复
     }
   }
 
