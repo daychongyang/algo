29290e129baad70d8095c203ae2657a20d6d68ce
daychongyang <daychongyang@gmail.com>
2021-03-23 22:32:36 +0800

    feat: #21 合并两个有序链表

--- leetcode/merge_two_sorted_lists.ts
+++ leetcode/merge_two_sorted_lists.ts
@@ -4,8 +4,8 @@ class ListNode {
   val: number;
   next: ListNode | null;
   constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
+    this.val = val ?? 0;
+    this.next = next ?? null;
   }
 }
 
@@ -13,5 +13,21 @@ export function mergeTwoLists(
   l1: ListNode | null,
   l2: ListNode | null
 ): ListNode | null {
-  return null;
+  const dummy = new ListNode();
+  let current = dummy;
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      current.next = l1;
+      l1 = l1?.next ?? null;
+    } else {
+      current.next = l2;
+      l2 = l2?.next ?? null;
+    }
+
+    current = current.next;
+  }
+
+  current.next = l1 ?? l2;
+
+  return dummy.next;
 }
